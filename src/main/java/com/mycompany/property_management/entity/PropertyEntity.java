47139bcac0ebe76bcb0a2821e0d9c4b32fc97f87
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2025-05-20 23:02:01 +0530

    Made the h2 DB setup for saving data. * Setup h2 properties * Entity and Repository setup

---
+++ src/main/java/com/mycompany/property_management/entity/PropertyEntity.java
@@ -0,0 +1,27 @@
+package com.mycompany.property_management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PROPERTY_TABLE")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PropertyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "PROPERTY_TITLE", nullable = false)
+    private String title;
+    private String description;
+    private String owner;
+    @Column(name = "EMAIL", nullable = true)
+    private String ownerEmail;
+    private Double price;
+    private String address;
+
+}
--- src/main/java/com/mycompany/property_management/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/mycompany/property_management/service/impl/PropertyServiceImpl.java
@@ -1,14 +1,29 @@
 package com.mycompany.property_management.service.impl;
 
 import com.mycompany.property_management.dto.PropertyDTO;
+import com.mycompany.property_management.entity.PropertyEntity;
+import com.mycompany.property_management.repository.PropertyRepository;
 import com.mycompany.property_management.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PropertyServiceImpl implements PropertyService {
 
+    @Autowired
+    private PropertyRepository repository;
+
     @Override
     public PropertyDTO saveProperty(PropertyDTO propertyDTO) {
+        PropertyEntity propertyEntity = new PropertyEntity();
+        propertyEntity.setTitle(propertyDTO.getTitle());
+        propertyEntity.setDescription(propertyDTO.getDescription());
+        propertyEntity.setAddress(propertyDTO.getAddress());
+        propertyEntity.setOwnerEmail(propertyDTO.getOwnerEmail());
+        propertyEntity.setOwner(propertyDTO.getOwner());
+        propertyEntity.setPrice(propertyDTO.getPrice());
+
+        repository.save(propertyEntity);
         return null;
     }
 
